44838a473632c6030a27affa9962322effd1afc9
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-06-28 11:39:44 +0530

    #include<stdio.h> int main() {     int n,i,t=0,j;     scanf("%d",&n);     for(i=2;i<n;i++)     {         if(n%i==0)         {             for(j=1;j<=i;j++)             {                 if(i%j==0)                 t++;             }             if(t==2)             {                printf("%d %d",i,n/i);                return 0;             }         }         if(i==n-1)         printf("-1");     }     return 0; }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -5,17 +5,17 @@ int main()
     scanf("%d",&n);
     for(i=2;i<n;i++)
     {
-        if(n % i==0)
+        if(n%i==0)
         {
             for(j=1;j<=i;j++)
             {
-                if(i % j==0)
+                if(i%j==0)
                 t++;
             }
             if(t==2)
             {
-                printf("%d %d",i,n / i);
-                return 0;
+               printf("%d %d",i,n/i);
+               return 0;
             }
         }
         if(i==n-1)
