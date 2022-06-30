bf53ee60cbab6991f790d6995bf058f79770ef28
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-07-01 00:35:51 +0530

    #include<stdio.h>
    int   main( )
    {
    int   beg, end, f, temp, i, j  ;
    scanf("%d", &beg) ;
    scanf("%d", &end) ;
    for (   j = beg ;  j <= end ;  j++ )
    {
    f = 0 ;
    for (i = 1 ;i < j ;i++) 
    {
    if (j % i == 0)
    f = f + 1 ;
    }
    if (f == 1)
    printf("%d
    ",j);
    }
    return 0;
    }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
-int main()
+
+
+int   main( )
 {
-    int fact,i,j,n1,n2;
-    scanf("%d%d",&n1,&n2);
-    for(i=n1;i<=n2;i++)
-    {
-        fact=0;
-        for(j=1;j<=i;j++)
-        {
-            if(i%j==0)
-            {
-                fact++;
-            }
-        }
-        if(fact==2)
-        {
-            printf("%d
-",i);
-        }
-    }
-    return 0;
+int   beg, end, f, temp, i, j  ;
+scanf("%d", &beg) ;
+scanf("%d", &end) ;
+for (   j = beg ;  j <= end ;  j++ )
+{
+f = 0 ;
+for (i = 1 ;i < j ;i++) 
+{
+if (j % i == 0)
+f = f + 1 ;
+}
+if (f == 1)
+printf("%d
+",j);
+}
+return 0;
 }
