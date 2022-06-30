b1c8014ef9181ed0f5b5e94eb38787f39059e6cc
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-07-01 00:27:00 +0530

    #include<stdio.h> int palin(int n){     int r,s=0,t=n;     while(n){         r=n%10;         s=s*10+r;         n=n/10;     }     if(t==s) return 1;     else return 0; } int isprime(int n){     if(n<=1) return 0;     for(int i=2;i*i<=n;i++){         if(n%i==0) return 0;     }     return 1; } int main(){     int n;     scanf("%d",&n);int t=n+1;     while(1){         if(palin(t)){             if(isprime(t)){                 printf("%d",t);                 break;             }         }         t++;     }     return 0; }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,36 +1,32 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,rev=0,r,m,temp,fact=0;
-    scanf("%d",&n);
-    i=n+1;
-    while(i>0)
-    {
-        rev=0;
-        temp=i;
-        fact=0;
-        while(temp>0)
-        {
-            r=temp%10;
-            rev=(rev*10)+r;
-            temp/=10;
-        }
-        if(i==rev)
-        {
-            for(m=1;m<=i;m++)
-            {
-                if(i%m==0)
-                {
-                    fact++;
-                }
-            }
-            if(fact==2)
-            {
-                printf("%d",i);
+int palin(int n){
+    int r,s=0,t=n;
+    while(n){
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
+    }
+    if(t==s) return 1;
+    else return 0;
+}
+int isprime(int n){
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main(){
+    int n;
+    scanf("%d",&n);int t=n+1;
+    while(1){
+        if(palin(t)){
+            if(isprime(t)){
+                printf("%d",t);
                 break;
             }
         }
-        i++;
+        t++;
     }
     return 0;
 }
