819ae822153d378bf65b18c92e2a506a821f063e
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-07-01 14:33:17 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d",&a[i]);     }     int m=n+2,c=0;     a[n]=a[0];     a[n+1]=a[1];     for(int i=0;i+2<m;i++){         if((a[i]%2==0 && a[i+2]%2!=0) || (a[i]%2!=0 && a[i+2]%2==0))         c++;     }     printf("%d",c);     return 0; }

--- count_elements_in_an_array_in_circular_form.c
+++ count_elements_in_an_array_in_circular_form.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
-int main()
-{
-    int i,n,a[100],c=0;
+int main(){
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
+    int a[n];
+    for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
+    int m=n+2,c=0;
     a[n]=a[0];
     a[n+1]=a[1];
-    for(i=1;i<=n;i++)
-    {
-        if(a[i-1]%2==0&&a[i+1]%2!=0)
-        {
-            c++;
-        }
-        if(a[i-1]%2!=0&&a[i+1]%2==0)
-        {
-            c++;
-        }
+    for(int i=0;i+2<m;i++){
+        if((a[i]%2==0 && a[i+2]%2!=0) || (a[i]%2!=0 && a[i+2]%2==0))
+        c++;
     }
     printf("%d",c);
+    return 0;
 }
