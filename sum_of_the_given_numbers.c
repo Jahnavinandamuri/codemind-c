537333ca08306dfe991eda3db5bda24990c1d9fb
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-07-01 11:31:40 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     while (n--){         int a,b;         scanf("%d %d",&a,&b);         printf("%d ",a+b);
        } return 0;
    }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int main()
-{
-    int n,a,b,i,sum=0;
+int main(){
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        sum=0;
-        scanf("%d%d",&a,&b);
-        sum=a+b;
+    while (n--){
+        int a,b;
+        scanf("%d %d",&a,&b);
         printf("%d
-",sum);
-    }
+",a+b);
+       
+    } return 0;
 }
+
+
