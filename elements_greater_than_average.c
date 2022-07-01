51411bd559eb90746ab5f7e1da9cadabf7f854b6
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-07-01 14:34:44 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n],s=0,f=0,sum=0;     for(int i=0;i<n;i++){     scanf("%d",&a[i]);     sum+=a[i];     }     int k;     scanf("%d",&k);     for(int i=0;i<n;i++){        if(a[i]>=sum/n)        s++;     }     printf("%d ",s);
         return 0;
    }

--- elements_greater_than_average.c
+++ elements_greater_than_average.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-int main()
-{
-    int ar[100],n,sum=0,c=0,i;
-    int a;
+int main(){
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&ar[i]);
-        sum=sum+ar[i];
+    int a[n],s=0,f=0,sum=0;
+    for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    sum+=a[i];
     }
-    a=(sum/n);
-    for(i=0;i<n;i++)
-    {
-        if(ar[i]>=a)
-        c++;
+    int k;
+    scanf("%d",&k);
+    for(int i=0;i<n;i++){
+       if(a[i]>=sum/n)
+       s++;
     }
-    printf("%d",c);
+    printf("%d ",s);
+   
+     return 0;
 }
