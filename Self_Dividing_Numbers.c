7fa4ae6ff029c7d83f74497ace0d5476471807cc
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-10-06 12:51:15 +0530

    #include<stdio.h> int self(int num) {     int flag=0;     int temp=num,r;     while (temp>0)     {         r=temp%10;         if (r==0)         {             flag=0;             break;         }         else         {             if (num%r==0)             {                 flag=1;             }             else             {                 flag=0;                 break;             }         }         temp=temp/10;     }     if (flag==1)     {         return 1;     }     else     {         return 0;     } } int main() {     int a,b;     scanf("%d%d",&a,&b);     for (a; a<=b; a++)     {         if (self(a)==1)         {             printf("%d ",a);         }     }     return 0; }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,23 +1,50 @@
 #include<stdio.h>
-int self(int n){
-    int r,t=n;
-    if(n<10) return 1;
-    if(n%10==0) return 0;
-    while(n){
-        r=n%10;
-        if(t%r!=0) return 0;
-        n/=10;
+int self(int num)
+{
+    int flag=0;
+    int temp=num,r;
+    while (temp>0)
+    {
+        r=temp%10;
+        if (r==0)
+        {
+            flag=0;
+            break;
+        }
+        else
+        {
+            if (num%r==0)
+            {
+                flag=1;
+            }
+            else
+            {
+                flag=0;
+                break;
+            }
+        }
+        temp=temp/10;
     }
-    return 1;
+    if (flag==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-int main(){
+int main()
+{
     int a,b;
-    scanf("%d
-%d",&a,&b);
-    for(int i=a;i<=b;i++){
-        if(self(i)){
-            printf("%d ",i);
+    scanf("%d%d",&a,&b);
+    for (a; a<=b; a++)
+    {
+        if (self(a)==1)
+        {
+            printf("%d ",a);
         }
     }
     return 0;
 }
+ 
