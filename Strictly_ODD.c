433704a8b9e16dc4c315f38e3e582d211cc96485
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-10-06 19:43:52 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     int flag=0;     for (i=0; i<a; i++)     {         if (arr[i]%2!=0 && i%2!=0)         {             flag=1;         }         else if (arr[i]%2!=0 && i%2==0)         {             flag=0;             break;         }     }     if (flag==1)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,24 +1,33 @@
 #include<stdio.h>
-int main(){
-    int n;scanf("%d",&n);
-    int a[n],s=0;
-    for(int i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    for(int i=0;i<n;i++){
-       if(a[i]%2==1){
-           if(i%2==1){
-           s=0;
-       }
-       else{
-           s=1;
-           printf("False");
-           break;
-       }
-           
-       } 
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
     }
-    if(s==0){
+    int flag=0;
+    for (i=0; i<a; i++)
+    {
+        if (arr[i]%2!=0 && i%2!=0)
+        {
+            flag=1;
+        }
+        else if (arr[i]%2!=0 && i%2==0)
+        {
+            flag=0;
+            break;
+        }
+    }
+    if (flag==1)
+    {
         printf("True");
     }
-   return 0;
+    else
+    {
+        printf("False");
+    }
+    return 0;
 }
