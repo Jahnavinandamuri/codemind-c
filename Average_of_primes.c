c87cfca74d0a5b04cc1b37abe22da17997c7a364
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-10-07 10:28:53 +0530

    #include<stdio.h> int prime(int num) {     int c=0,i;     for (i=1; i<=num; i++)     {         if (num%i==0)         {             c++;         }     }     if (c==2)     {         return 1;     }     return 0; } int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     int pc=0,ps=0;     for (i=0; i<a; i++)     {         if (prime(arr[i])==1)         {             ps+=arr[i];             pc++;         }     }     double avg=(ps*1.0)/pc;     printf("%.2lf",avg);     return 0; }

--- Average_of_primes.c
+++ Average_of_primes.c
@@ -1,25 +1,39 @@
 #include<stdio.h>
-int isPrime(int n){
-    if(n<=1) return 0;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) return 0;
-    }
-    return 1;
-}
-int main(){
-    int n,sum=0,c=0;
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++)
-    scanf("%d ",&a[i]);
-    for(int i=0;i<n;i++)
+int prime(int num)
+{
+    int c=0,i;
+    for (i=1; i<=num; i++)
     {
-        if(isPrime(a[i])){
-            sum+=a[i];
+        if (num%i==0)
+        {
             c++;
         }
     }
-    float avg=(float)sum/c;
-    printf("%.2f",avg);
+    if (c==2)
+    {
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int pc=0,ps=0;
+    for (i=0; i<a; i++)
+    {
+        if (prime(arr[i])==1)
+        {
+            ps+=arr[i];
+            pc++;
+        }
+    }
+    double avg=(ps*1.0)/pc;
+    printf("%.2lf",avg);
     return 0;
 }
