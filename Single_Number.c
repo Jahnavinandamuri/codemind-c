cc296b4a6cb7010062a9e5b456d8edf1089953b6
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-10-12 00:15:00 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n],ans=0;     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);         ans^=a[i];     }     printf("%d",ans);     return 0;
    }

---
+++ Single_Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],ans=0;
+    for(int i=0;i<n;i++) 
+    {
+        scanf("%d",&a[i]);
+        ans^=a[i];
+    }
+    printf("%d",ans);
+    return 0;
+    
+}
