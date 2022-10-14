b15514abe2fe33261b008d8c347ae50b2e6ad028
Jahnavinandamuri <97595357+Jahnavinandamuri@users.noreply.github.com>
2022-10-14 17:24:17 +0530

    #include <stdio.h> int main() {      int a[100][100],i,n,m,j,sum=0,max=0;      scanf("%d%d",&n,&m);      for(i=0;i<n;i++)      {           for(j=0;j<m;j++)           {                scanf("%d",&a[i][j]);           }      }      for(i=0;i<n;i++)      {
            for(j=0;j<m;j++)
            {
                if((i==0) || (i==n-1) || (j==0) || (j==m-1))
                {
                    sum=sum+a[i][j]; 
                }
            }
            
         } 
         printf("%d ",sum);
    }

--- Sum_of_all_boundary_values_in_a_matrix.c
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -1,21 +1,26 @@
-#include<stdio.h>
-int main(){
-    int n,m;
-    scanf("%d %d",&n,&m);
-    int a[n][m];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            scanf("%d ",&a[i][j]);
+#include <stdio.h>
+int main()
+{
+     int a[100][100],i,n,m,j,sum=0,max=0;
+     scanf("%d%d",&n,&m);
+     for(i=0;i<n;i++)
+     {
+          for(j=0;j<m;j++)
+          {
+               scanf("%d",&a[i][j]);
+          }
+     }      
+     for(i=0;i<n;i++)
+     {
+        
+        for(j=0;j<m;j++)
+        {
+            if((i==0) || (i==n-1) || (j==0) || (j==m-1))
+            {
+                sum=sum+a[i][j]; 
+            }
         }
-    }
-    int sum=0,sum1=0;
-   
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==0 || j==0 || i==n-1|| j==m-1)
-            sum1+=a[i][j];
-        }
-    }
-    printf("%d",sum1);
-    return 0;
+        
+     } 
+     printf("%d ",sum);
 }
